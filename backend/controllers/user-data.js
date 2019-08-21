d6eaf3a664de46ebcfb23583e8971ff0cae97e79
Girum Hagos <hagosgirum@gmail.com>
2019-08-20 22:58:25 -0400

    Added an async function to get test data to see if we get results

--- backend/controllers/user-data.js
+++ backend/controllers/user-data.js
@@ -1,3 +1,19 @@
-exports.getUserData = (req, res, next) => {
+const http = require("http");
 
+async function getData() {
+  const url = "https://s3-eu-west-1.amazonaws.com/sentiance.solutions/datasets/public/user1.json";
+  const result = await http.get(url);
+
+  return result;
+  console.dir(result);
+}
+
+exports.getUserData = (req, res, next) => {
+  getData()
+  .then(result => {
+    console.log(result);
+  })
+  .catch(err => {
+    console.log(err);
+  })
 };
