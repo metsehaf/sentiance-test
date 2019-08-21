117ca3563da710dbd0be0ed7010a7af74fcc5775
Girum Hagos <hagosgirum@gmail.com>
2019-08-21 19:24:25 -0400

    Sending the response from nodejs to client application (Angular)

--- backend/controllers/user-data.js
+++ backend/controllers/user-data.js
@@ -1,20 +1,15 @@
-const https = require("https");
-
-// return async function from fetching api to use in promise
-async function getData() {
-  const url = "https://s3-eu-west-1.amazonaws.com/sentiance.solutions/datasets/public/user1.json";
-  const result = await https.get(url);
-
-  return result;
-  console.dir(result);
-}
+const axios = require('axios');
 
 exports.getUserData = (req, res, next) => {
-  getData()
-  .then(result => {
-    console.log(result);
+  axios.get("https://s3-eu-west-1.amazonaws.com/sentiance.solutions/datasets/public/user1.json")
+  .then(response => {
+    console.log(response);
+    res.status(200).json({
+      message: "success, fetching user data Succeeded",
+      userData: response.data
+    })
   })
-  .catch(err => {
-    console.log(err);
-  })
+  .catch(error => {
+    console.log(error);
+  });
 };
